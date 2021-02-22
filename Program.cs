bd896856446fdcd7698af724616b75e5f3496e79
Hiram Hernandez <ehiram.hr@gmail.com>
2021-02-22 09:35:32 -0700

    Se agrega funcionalidad para seleccionar opción a jugar.

--- Program.cs
+++ Program.cs
@@ -4,9 +4,36 @@ namespace RockPaperScissors
 {
     class Program
     {
+
+        /// <summary>
+        /// Solicita al usuario una opción a jugar.
+        /// </summary>
+        /// <returns>La opción seleccionada: 1 - piedra, 2 - papel, 3 - tijera</returns>
+        static int SelectOption()
+        {
+            Console.WriteLine("Selecciona la opción a jugar:");
+            Console.WriteLine("1) Piedra");
+            Console.WriteLine("2) Papel");
+            Console.WriteLine("3) Tijera");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+            return option;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Seleccionar opción a jugar (piedra, papel o tijera)
+            // 1: piedra
+            // 2: papel
+            // 3: tijera
+            int option = SelectOption();
+            Console.WriteLine("Opción seleccionada: " + option);
+
+            //TODO: La computadora genera su opción a jugar de manera aleatoria (piedra, papel o tijera)
+
+            //TODO: Determinar quién ganó de acuerdo a las opciones seleccionadas, y mostrar al ganador
+
+            //TODO: Jugar 3 partidas, y mencionar quién ganó al final (jugador o computadora)
         }
     }
 }
